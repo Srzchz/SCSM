043da43a7a3957479e67776928c2792b455482b0
pxltl <chocolatemallows999@gmail.com>
2026-07-20 11:17:31 +0800

    Removed temporary files

--- __REMOVE__zip_file/crm-refactored/crm_refactored/routes/communication-logs.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Modules\CommunicationLogs\Controllers\CommunicationLogController;
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('communication-logs')->group(function () {
-    Route::get('/', [CommunicationLogController::class, 'index'])->name('communication-logs');
-});
