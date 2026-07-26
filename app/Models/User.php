8fdf5591341995f31e32ff15cdbc8a8398a7d720
hexxa69 <johnyevsbryan.suico@cvsu.edu.ph>
2026-07-26 21:40:15 +0800

    may naiwan

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,12 +12,25 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role', 'department', 'avatar_initials'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    public const ROLE_MANAGER = 'manager';
+    public const ROLE_EMPLOYEE = 'employee';
+
+    public function isManager(): bool
+    {
+        return $this->role === self::ROLE_MANAGER;
+    }
+
+    public function isEmployee(): bool
+    {
+        return $this->role === self::ROLE_EMPLOYEE;
+    }
 
     /**
      * Get the attributes that should be cast.
